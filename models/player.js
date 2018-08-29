791e9fc0beffdaa875ca9230e933e28a2f4696ff
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-08-29 14:24:16 -0400

    playerStats get

--- models/player.js
+++ models/player.js
@@ -7,7 +7,16 @@ var PlayerSchema = new Schema({
     dateCreated: Date,
     email: {type: String, required:true, index: { unique: true }},
     password: String,
-    roles: String
+    roles: String,
+    teams: [{ type: Schema.Types.ObjectId, ref: 'Team' }]
+}, { toJSON: { virtuals: true } });
+
+PlayerSchema.virtual('teamPlayers',{
+    ref: 'Team',
+    localField: '_id',
+    foreignField: 'players',
+    justOne:false
+
 });
 
 module.exports = mongoose.model('Player', PlayerSchema);
