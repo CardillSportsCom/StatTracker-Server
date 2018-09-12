380da394d337751a1b4ebea1ff4e5d38a856cb8a
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-09-12 18:08:07 -0400

    score controller

--- src/routes.js
+++ src/routes.js
@@ -7,47 +7,8 @@ var TeamTypeRoutes = require('../lib/teamType/routes.js');
 var StatRoutes = require('../lib/stat/routes.js');
 var PlayerStatRoutes = require('../lib/playerStat/routes.js');
 var GameStatRoutes = require('../lib/gameStat/routes.js');
+var GameRoutes = require('../lib/game/routes.js');
+var ScoreRoutes = require('../lib/score/routes.js');
 
 
-module.exports =[].concat(LeagueRoutes, PlayerRoutes, LeaguePlayerRoutes, LeagueTypeRoutes, TeamTypeRoutes, StatRoutes, PlayerStatRoutes, GameStatRoutes);
-
-// module.exports = [
-   
-//     // {
-//     //     method: 'PUT',
-//     //     path: '/addPlayer/{leagueId}',
-//     //     options: {
-//     //         handler: LeagueController.addplayer,
-//     //         description: 'Add Player to league',
-//     //         notes: 'Adds player to league',
-//     //         tags: ['api', 'league'],
-//     //         validate: {
-//     //             params: Joi.object({
-//     //                 leagueId: Joi.string().required().description('id of the league')
-//     //             }),
-//     //             payload: Joi.object({
-//     //                 playerId: Joi.string().required().description('player id')
-//     //             })
-//     //         }
-//     //     }
-//     // },
-//     // {
-//     //     method: 'DELETE',
-//     //     path: '/removePlayer/{leagueId}',
-//     //     options: {
-//     //         handler: LeagueController.removePlayer,
-//     //         description: 'Remove player from league',
-//     //         notes: 'Removes players from league',
-//     //         tags: ['api', 'league'],
-//     //         validate: {
-//     //             params: Joi.object({
-//     //                 leagueId: Joi.string().required().description('id of the league')
-//     //             }),
-//     //             payload: Joi.object({
-//     //                 playerId: Joi.string().required().description('player id')
-//     //             })
-//     //         }
-//     //     }
-//     // }
-
-// ];
+module.exports =[].concat(LeagueRoutes, PlayerRoutes, GameRoutes, LeaguePlayerRoutes, LeagueTypeRoutes, TeamTypeRoutes, StatRoutes, PlayerStatRoutes, GameStatRoutes, ScoreRoutes);
