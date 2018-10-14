56ec1c45fbf13480398d9aefe5087e80c4b6fa3c
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-10-14 19:24:15 -0400

    login and auth set up

--- src/database.js
+++ src/database.js
@@ -1,8 +1,7 @@
 var Mongoose = require('mongoose');
 //load database
-// Mongoose.connect('mongodb://localhost/hapijs-mongoose-restapi/');
-Mongoose.connect('mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker');
-// Mongoose.connect('mongodb://testcstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker');
+// Mongoose.connect('mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker');
+Mongoose.connect('mongodb://testcstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker');
 
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
