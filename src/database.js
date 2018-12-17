e36399fa591e89002b5f27be4d13d08a6d5ffb7e
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-12-17 01:30:15 -0500

    average stats

--- src/database.js
+++ src/database.js
@@ -2,8 +2,8 @@ var Mongoose = require('mongoose');
 //load database
 // Mongoose.connect('mongodb://desktop-osq5g16:3000/hapijs-mongoose-restapi');
 
-// Mongoose.connect('mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker');
-Mongoose.connect('mongodb://testcstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker');
+Mongoose.connect('mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker');
+// Mongoose.connect('mongodb://testcstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker');
 
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
