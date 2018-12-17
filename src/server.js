f7cc8b45ec14d9f272ff4125ac1ff0093c914c37
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-12-17 01:26:35 -0500

    average stats

--- src/server.js
+++ src/server.js
@@ -45,15 +45,15 @@ const serviceAccount = require('./serviceAccount.json');
             },
             security: [{ 'jwt': [] }],
             // auth: 'jwt',
-        //    host:'test-cardillsports-stattracker.herokuapp.com'
-           host:'api-cardillsports-st.herokuapp.com'
+           host:'test-cardillsports-stattracker.herokuapp.com'
+        //    host:'api-cardillsports-st.herokuapp.com'
            
         
     };
     const mongoOptions = {
         promises: 'native',
-        uri: 'mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker'
-        // uri: 'mongodb://testcsstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker'
+        // uri: 'mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker'
+        uri: 'mongodb://testcsstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker'
         // uri: 'mongodb://desktop-osq5g16:3000/hapijs-mongoose-restapi'
         
         
