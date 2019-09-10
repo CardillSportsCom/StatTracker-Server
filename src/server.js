e6ae4f7860e633f34dc687585f36a3ead432c2e0
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2019-09-10 16:52:07 -0400

    push to production three pointers

--- src/server.js
+++ src/server.js
@@ -55,8 +55,8 @@ const serviceAccount = require('./serviceAccount.json');
             },
             security: [{ 'jwt': [] }],
             // Comment this out for local development
-            host:'test-cardillsports-stattracker.herokuapp.com'
-            // host:'api-cardillsports-st.herokuapp.com'
+            // host:'test-cardillsports-stattracker.herokuapp.com'
+            host:'api-cardillsports-st.herokuapp.com'
     };
     const mongoOptions = {
         promises: 'native',
