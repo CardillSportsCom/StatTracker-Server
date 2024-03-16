49719c74d7816bde5166348e18268bd338216bbb
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2024-03-15 23:07:44 -0400

    add player to video highlights

--- models/videoHighlights.js
+++ models/videoHighlights.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 var VideoHighlightSchema = new Schema({
   dateCreated: Date,
   storageKey: String,
+  player: { type: Schema.Types.ObjectId, ref: "Player" },
 });
 
 module.exports = mongoose.model(
