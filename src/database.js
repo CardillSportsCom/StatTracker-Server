938a85ead1f9a69f5416c580ce3c93ad69d1114f
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2024-04-10 00:10:14 -0400

    format database.js file

--- src/database.js
+++ src/database.js
@@ -1,17 +1,19 @@
-var Mongoose = require('mongoose');
+var Mongoose = require("mongoose");
 //load database
 // Mongoose.connect('mongodb://desktop-osq5g16:3000/hapijs-mongoose-restapi');
 
 // Mongoose.connect('mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker');
 // Mongoose.connect('mongodb://testcstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker');
 // Mongoose.connect('mongodb+srv://cardill-sports:Cardillsports@cardill-sports-qg6aw.mongodb.net/', {dbName: 'testcsstattracker'});
-Mongoose.connect('mongodb+srv://cardill-sports:Cardillsports@cardill-sports-qg6aw.mongodb.net/', {dbName: 'csstattracker'});
-
+Mongoose.connect(
+  "mongodb+srv://cardill-sports:Cardillsports@cardill-sports-qg6aw.mongodb.net/",
+  { dbName: "csstattracker" }
+);
 
 var db = Mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function callback() {
-    console.log('Connection with database succeeded.');
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function callback() {
+  console.log("Connection with database succeeded.");
 });
 exports.db = db;
 // const MongoClient = require('mongodb').MongoClient;
