df9de84faaa0732a1b7d762a0f68640ac739f3f0
jasonrajasegaram <jason.rajasegaram19@gmail.com>
2024-04-17 20:37:12 -0400

    Add Get Player By Email

--- src/database.js
+++ src/database.js
@@ -5,15 +5,12 @@ var Mongoose = require("mongoose");
 // Mongoose.connect('mongodb://csstattracker:Mr1aB-09d3U-@den1.mongo1.gear.host:27001/csstattracker');
 // Mongoose.connect('mongodb://testcstattracker:Yf70c43-w48-@den1.mongo1.gear.host:27001/testcstattracker');
 // Mongoose.connect('mongodb+srv://cardill-sports:Cardillsports@cardill-sports-qg6aw.mongodb.net/', {dbName: 'testcsstattracker'});
-Mongoose.connect(
-  "mongodb+srv://cardill-sports:Cardillsports@cardill-sports-qg6aw.mongodb.net/",
-  { dbName: "csstattracker" }
-);
+Mongoose.connect("mongodb+srv://cardill-sports:Cardillsports@cardill-sports-qg6aw.mongodb.net/", { dbName: "csstattracker" });
 
 var db = Mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function callback() {
-  console.log("Connection with database succeeded.");
+	console.log("Connection with database succeeded.");
 });
 exports.db = db;
 // const MongoClient = require('mongodb').MongoClient;
--- src/server.js
+++ src/server.js
@@ -18,82 +18,82 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccount.json");
 
 (async () => {
-  var port = parseInt(process.env.PORT) || 3000;
-  const server = new Hapi.Server({
-    port: port,
-    // Uncomment this to do local development
-    // host: 'localhost',
-    routes: {
-      cors: {
-        origin: ["*"],
-        headers: ["Accept", "Content-Type", "Authorization"],
-        additionalHeaders: ["X-Requested-With"],
-      },
-      // },
-      // validate: {
-      //     failAction: async (request, h, err) => {
-      //         // During development, log and respond with the full error.
-      //         console.error(err);
-      //         throw err;
-      //     }
-      //   }
-    },
-  });
-  server.port = port;
-  const swaggerOptions = {
-    grouping: "tags",
-    info: {
-      title: "Test API Documentation",
-      version: Pack.version,
-    },
-    securityDefinitions: {
-      jwt: {
-        type: "apiKey",
-        name: "Authorization",
-        in: "header",
-      },
-    },
-    security: [{ jwt: [] }],
-    // Comment this out for local development
-    // Not specifying the host will default to localhost
+	var port = parseInt(process.env.PORT) || 3000;
+	const server = new Hapi.Server({
+		port: port,
+		// Uncomment this to do local development
+		// host: 'localhost',
+		routes: {
+			cors: {
+				origin: ["*"],
+				headers: ["Accept", "Content-Type", "Authorization"],
+				additionalHeaders: ["X-Requested-With"],
+			},
+			// },
+			// validate: {
+			//     failAction: async (request, h, err) => {
+			//         // During development, log and respond with the full error.
+			//         console.error(err);
+			//         throw err;
+			//     }
+			//   }
+		},
+	});
+	server.port = port;
+	const swaggerOptions = {
+		grouping: "tags",
+		info: {
+			title: "Test API Documentation",
+			version: Pack.version,
+		},
+		securityDefinitions: {
+			jwt: {
+				type: "apiKey",
+				name: "Authorization",
+				in: "header",
+			},
+		},
+		security: [{ jwt: [] }],
+		// Comment this out for local development
+		// Not specifying the host will default to localhost
 
-    // host:'test-cardillsports-stattracker.herokuapp.com'
-    host: "api-cardillsports-st.herokuapp.com",
-  };
-  const mongoOptions = {
-    promises: "native",
-  };
+		// host:'test-cardillsports-stattracker.herokuapp.com'
+		// host: "api-cardillsports-st.herokuapp.com",
+	};
+	const mongoOptions = {
+		promises: "native",
+	};
 
-  server.register(HapiJWT);
-  server.auth.strategy("jwt", "jwt", {
-    key: secret,
-    validate: validate,
-    verifyOptions: {
-      algorithms: ["HS256"],
-      error: validateError,
-    },
-  });
-  // server.auth.default('jwt');
+	server.register(HapiJWT);
+	server.auth.strategy("jwt", "jwt", {
+		key: secret,
+		validate: validate,
+		verifyOptions: {
+			algorithms: ["HS256"],
+			error: validateError,
+		},
+	});
+	// server.auth.default('jwt');
 
-  await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: swaggerOptions,
-    },
-  ]);
+	await server.register([
+		Inert,
+		Vision,
+		{
+			plugin: HapiSwagger,
+			options: swaggerOptions,
+		},
+	]);
 
-  try {
-    await server.start();
-    console.log("Server running at:", server.info.uri);
-  } catch (err) {
-    console.log(err);
-  }
-  server.route(routes);
+	try {
+		await server.start();
+		console.log("Server running at:", server.info.uri);
+	} catch (err) {
+		console.log(err);
+	}
+	server.route(routes);
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://stat-tracker-1537117819639.firebaseio.com/",
-  });
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+		databaseURL: "https://stat-tracker-1537117819639.firebaseio.com/",
+	});
 })();
