a6ab821b6036bcf5cd8043853534950e97f880b7
jasonrajasegaram <jason.rajasegaram19@gmail.com>
2025-05-01 14:52:34 -0400

    fix how game stat gets separated by team

--- models/game.js
+++ models/game.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var GameSchema = new Schema({
-  teamA: { type: Schema.Types.ObjectId, ref: "Team" },
-  teamB: { type: Schema.Types.ObjectId, ref: "Team" },
-  teamAScore: String,
-  teamBScore: String,
-  type: { type: String, ref: "TeamType" },
-  league: { type: Schema.Types.ObjectId, ref: "League" },
-  season: { type: Schema.Types.ObjectId, ref: "Season" },
-  isOvertime: Boolean,
-  gameDuration: Number,
-  dateCreated: Date,
+	teamA: { type: Schema.Types.ObjectId, ref: "Team" },
+	teamB: { type: Schema.Types.ObjectId, ref: "Team" },
+	teamAScore: String,
+	teamBScore: String,
+	type: { type: Schema.Types.ObjectId, ref: "TeamType" },
+	league: { type: Schema.Types.ObjectId, ref: "League" },
+	season: { type: Schema.Types.ObjectId, ref: "Season" },
+	isOvertime: Boolean,
+	gameDuration: Number,
+	dateCreated: Date,
 });
 
 module.exports = mongoose.model("Game", GameSchema);
